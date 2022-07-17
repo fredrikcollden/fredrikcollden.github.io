d6f7a574b01fd04477db9d7ef186695a26d4e507
fredrikcollden <fredrik.collden@gmail.com>
2022-07-17 09:04:55 +0200

    Add Scss support with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: [
+        __dirname + '/src/main.js',
+        __dirname + '/src/main.scss'
+    ],
+    output: {
+        filename: 'js/main.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: [],
+            }, {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'css/', name: '[name].min.css'}
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
 };
